cd085299abaf0a5c4064dcd528284e083df194e9
yifei-fu <yifeifu2@gmail.com>
2018-08-29 23:52:43 +0800

    Post list API.

--- src/api/post.js
+++ src/api/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import {base_url} from './base_url'
-import {to_query_string} from './query'
+import { base_url } from './base_url'
+import { to_query_string } from './query'
 
 export function fetch_post_by_id(id) {
     return axios.request({
@@ -9,12 +9,43 @@ export function fetch_post_by_id(id) {
     })
 }
 
-export function fetch_post_list(query) {
-    if(typeof(query)==='object'){
+export function fetch_post_list(query, callback, limit) {
+    // callback accepts two params: status ('success' or 'error') and data (all loaded data)
+    if (typeof (query) === 'object') {
         query = to_query_string(query)
     }
-    return axios.request({
-        url: base_url + `/post/post/?${query}/`,
-        method: 'get'
-    })
+
+    if (typeof limit === 'undefined')
+        limit = 10
+    console.log(callback)
+    var api = {
+        next_url: base_url + `/post/post/?${query}&limit=${limit}/`,
+        count: 0,
+        data: [],
+        callback,
+        status: 'ready',
+        has_next() {
+            return !!this.next_url
+        },
+        get_next() {
+            this.status = 'loading'
+            axios.request({
+                url: this.next_url,
+                method: 'get'
+            }).then(response => {
+                this.status = 'ready'
+                this.count = response.data.count
+                this.next_url = response.data.next
+                this.data = this.data.concat(response.data.results)
+                this.callback('success', this.data)
+            })
+                .catch(error => {
+                    this.status = 'error'
+                    console.log(error)
+                    this.callback('error', this.data)
+                })
+
+        }
+    }
+    return api
 }
